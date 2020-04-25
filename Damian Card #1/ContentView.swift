d81c4b17f8ccf584ec3c912cfeed4803f3bbbb3a
Damian <d.medarov@me.com>
2020-04-26 01:22:04 +0300

    Initial commit

--- Damian Card #1/ContentView.swift
+++ Damian Card #1/ContentView.swift
@@ -8,14 +8,21 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        Text("Hello, World!")
+    struct ContentView: View {
+        var body: some View {
+            ZStack {
+                Color(red: 0.09, green: 0.63, blue: 0.52)
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                Text("Damian Medarov")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+            }
     }
-}
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
