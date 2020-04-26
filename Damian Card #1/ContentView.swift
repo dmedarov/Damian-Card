7b2d17860d02af9d2a0e9573638a2e569e7116a3
Damian <d.medarov@me.com>
2020-04-26 04:02:02 +0300

    InfoView added and extracted in new structure

--- Damian Card #1/ContentView.swift
+++ Damian Card #1/ContentView.swift
@@ -1,6 +1,6 @@
 //
 //  ContentView.swift
-//  Damian Card #1
+//  Damian Card
 //
 //  Created by Damian Medarov on 26.04.20.
 //  Copyright © 2020 Damian Medarov. All rights reserved.
@@ -8,16 +8,32 @@
 
 import SwiftUI
 
-    struct ContentView: View {
-        var body: some View {
-            ZStack {
-                Color(red: 0.09, green: 0.63, blue: 0.52)
-                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color(red: 0.09, green: 0.63, blue: 0.52)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Image("damian")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 150.0, height: 150.0)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                    .overlay(
+                        Circle().stroke(Color.white, lineWidth: 5))
                 Text("Damian Medarov")
                     .font(Font.custom("Pacifico-Regular", size: 40))
                     .fontWeight(.bold)
                     .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+                Divider()
+                InfoView(text: "+359 887301419", imageName: "phone.fill")
+                InfoView(text: "d.medarov@me.com", imageName: "envelop.fill")
             }
+            
+        }
     }
 
     struct ContentView_Previews: PreviewProvider {
@@ -26,3 +42,4 @@ import SwiftUI
         }
     }
 }
+
---
+++ Damian Card #1/InfoView.swift
@@ -0,0 +1,36 @@
+//
+//  InfoView.swift
+//  Damian Card #1
+//
+//  Created by Damian Medarov on 26.04.20.
+//  Copyright © 2020 Damian Medarov. All rights reserved.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    
+    let text: String
+    let imageName: String
+    
+    var body: some View {
+        RoundedRectangle(cornerRadius: 25)
+            .fill(Color.white)
+            .frame(height: 50)
+            .overlay(HStack {
+                Image(systemName: imageName)
+                    .foregroundColor(.green)
+                Text(text)
+            })
+            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+    }
+}
+
+
+
+struct InfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        InfoView(text: "Hello", imageName: "phone.fill")
+            .previewLayout(.sizeThatFits)
+    }
+}
